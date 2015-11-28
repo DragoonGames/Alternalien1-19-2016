edf754950647bca5cd736e82d2f2745b871b4d4a
punkasaurus <erincfink@gmail.com>
2015-11-28 16:18:24 -0600

    Main Menu finished
    Adjust Main Menu text to accommodate various screen sizes

--- Assets/GUISkin/GUISample.cs
+++ Assets/GUISkin/GUISample.cs
@@ -6,19 +6,32 @@ public class GUISample : MonoBehaviour
 	//public Texture2D image;
 	public string text = "";
 	public GUISkin mySkin;
-	int buttonW = 100;
-	int buttonH = 16;
+	float buttonW = Screen.width/8;
+	float buttonH = Screen.height/20;
+	//public int fontSize;
 	
 	void OnGUI() {
 		GUI.skin = mySkin;
-		if (gameObject.tag == "alienData") {
-			print ("still alive");
-			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) - 20, buttonW, buttonH), text))
-				print ("button works");
+		mySkin.button.fontSize = (int)(buttonH / 1.5);
+		if (Screen.width > 1000)
+			buttonW = Screen.width/5;
+
+		if (gameObject.tag == "AlienData") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) - (buttonH * 2), buttonW, buttonH), text))
+				Application.LoadLevel(1);
 		}
-		if (gameObject.tag == "personnel") {
-			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) , buttonW, buttonH), text))
-				print ("second button works");
+		if (gameObject.tag == "Personnel") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2), buttonW, buttonH), text))
+				Application.LoadLevel(2);
+		}
+		if (gameObject.tag == "LevelSelect") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) + (buttonH * 2), buttonW, buttonH), text))
+				Application.LoadLevel(3);
+		}
+		if (gameObject.tag == "Exit") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 4), (Screen.height / 3) + Screen.height / 2, buttonW/2, buttonH), text))
+				Application.Quit();
+
 		}
 	} 
 }
