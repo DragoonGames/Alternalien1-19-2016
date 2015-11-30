eae5d360fd640352a42a9d37ee0bab8e2ba7ea06
punkasaurus <finke@MM172-900373D7.ad.matc.edu>
2015-11-30 11:40:51 -0600

    Alien Bios Started, unfinished

---
+++ Assets/Scripts/LevelScripts/LargeFiles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LargeFiles : MonoBehaviour {
+
+	Vector3 size = Vector3.zero;
+
+	void Update(){
+		size = new Vector3 (Screen.width / 2, Screen.height / 2, 0);
+		//transform.
+	}
+
+}
---
+++ Assets/bioClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bioClick : MonoBehaviour {
+
+	string obj;
+
+	void Update () {
+		if (Input.GetMouseButtonUp (0)){
+			gameObject.Find(gameObject.tag).GetComponent(
+		}
+
+	}
+}
