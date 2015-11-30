819fde976bb224dba8d46d8afda42e3d69df08c1
punkasaurus <erincfink@gmail.com>
2015-11-29 22:11:31 -0600

    adding sound fx

--- Assets/Scripts/GameState/LevelUnlock.cs
+++ Assets/Scripts/GameState/LevelUnlock.cs
@@ -1,27 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class LevelUnlock : MonoBehaviour {
 
 	public Camera mainCamera;
 	public string[] levels;
+	public GUISkin mySkin;
+	int buttonW = 100;
+	int buttonH = 25;
 	bool label = false;
 	void OnGUI()
 	{
-		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 180) / 2, 100, 20), "Level 1")) {
+		GUI.skin = mySkin;
+		mySkin.button.fontSize = (int)(buttonH / 1.5);
+		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 180) / 2, buttonW, buttonH), "Tutorial")) {
 			Application.LoadLevel(levels[0]);
 		}
-		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 130) / 2, 100, 20), "Level 2")) {
+		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 100) / 2, buttonW, buttonH * 2), "Second\nTutorial")) {
 			if (PlayerPrefs.HasKey("unlockLevel2")) {
-				print ("Has unlockLevel2 key");
-				//Application.LoadLevel (levels[1]);
+				Application.LoadLevel (levels[1]);
 			}
 			else 
 			{
 				label = true;
 			}
 		}
-		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight - 80) / 2, 100, 20), "Level 3")) {
+		if (GUI.Button (new Rect ((mainCamera.pixelWidth - 100) / 2, (mainCamera.pixelHeight + 20) / 2, buttonW, buttonH), "Level 3")) {
 			if (PlayerPrefs.HasKey("unlockLevel3")) {
 				print ("Has unlockLevel3 key");
 				//Application.LoadLevel (levels[2]);
