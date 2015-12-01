6f9274379fb11e33466d88c71fa5be9decda55c9
punkasaurus <erincfink@gmail.com>
2015-11-30 20:40:37 -0600

    Start Image rect for alienbio

--- Assets/bioClick.cs
+++ Assets/bioClick.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class bioClick : MonoBehaviour {
 
-	string obj;
+	public Texture image;
+	private Texture _image;
+
+	void Start(){
+		_image.enabled = false;
+	}
 
 	void Update () {
-		if (Input.GetMouseButtonUp (0)){
-			gameObject.Find(gameObject.tag).GetComponent(
+		if (Input.GetMouseButtonUp (0)) {
+			if(_image.enabled = false)
+				_image.enabled = true;
+			else if (_image.enabled = true)
+				_image.enabled = false;
 		}
+	}
 
+	void OnGUI(){
+		_image = GUI.DrawTexture(new Rect (0, 0, 60, 60), image, ScaleMode.ScaleToFit);
 	}
 }
