08cadc6d3dedbca5bc6c8d3864283cd69e02a933
punkasaurus <erincfink@gmail.com>
2015-12-06 16:42:56 -0600

    Add Intro, Angry Sun, and Jungle

--- Assets/GUISkin/GUISample.cs
+++ Assets/GUISkin/GUISample.cs
@@ -16,17 +16,13 @@ public class GUISample : MonoBehaviour
 		if (Screen.width > 1000)
 			buttonW = Screen.width/5;
 
-		if (gameObject.tag == "AlienData") {
-			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) - (buttonH * 2), buttonW, buttonH), text))
-				Application.LoadLevel(1);
-		}
-		if (gameObject.tag == "Personnel") {
-			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2), buttonW, buttonH), text))
-				Application.LoadLevel(2);
-		}
-		if (gameObject.tag == "LevelSelect") {
-			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) + (buttonH * 2), buttonW, buttonH), text))
+		if (gameObject.tag == "Start") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), Screen.height/2, buttonW, buttonH), text))
 				Application.LoadLevel(3);
+		}
+		if (gameObject.tag == "AlienData") {
+			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 2), (Screen.height / 2) + (buttonH * 2), buttonW, buttonH), text))
+				Application.LoadLevel(2);
 		}
 		if (gameObject.tag == "Exit") {
 			if (GUI.Button (new Rect ((Screen.width / 2) - (buttonW / 4), (Screen.height / 3) + Screen.height / 2, buttonW/2, buttonH), text))
---
+++ Assets/Scripts/LevelScripts/ExitIntroScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitIntroScene : MonoBehaviour {
+
+	void Start(){
+		StartCoroutine (EndIntro());
+	}
+
+	IEnumerator EndIntro(){
+		print ("Intro Starts");
+		yield return new WaitForSeconds (3);
+		Application.LoadLevel(1);
+	}
+}
