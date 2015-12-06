87a1c6d6a2aa7349f49a478525eebd301a4256a1
punkasaurus <erincfink@gmail.com>
2015-12-06 15:39:35 -0600

    Update Scripts, add Watson

--- Assets/Scripts/LevelScripts/bioClick.cs
+++ Assets/Scripts/LevelScripts/bioClick.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class bioClick : MonoBehaviour {
 
 	public SpriteRenderer image;
-    //bool isDisabled;
     bool isFullScreen;
     Quaternion storedRotation;
     Vector3 storedPosition;
@@ -14,7 +13,6 @@ public class bioClick : MonoBehaviour {
 	Vector3 newPosition;
 	void Start(){
         image = GetComponent<SpriteRenderer>();
-        //isDisabled = false;
         isFullScreen = false;
         storedRotation = GetComponent<Transform>().rotation;
         storedPosition = GetComponent<Transform>().position;
@@ -28,23 +26,17 @@ public class bioClick : MonoBehaviour {
 	{
         if (!isFullScreen)
         {
-            print(gameObject);
-            //image.enabled = false;
             transform.position = new Vector3(0, 0, -9);
             transform.localScale = new Vector3(20, 20, 0);
             transform.rotation = new Quaternion(0, 0, 0, 0);
             isFullScreen = true;
-            //isDisabled = true;
         }
         else
         {
-            print(gameObject);
             isFullScreen = false;
             transform.position = storedPosition;
             transform.localScale = storedScale;
             transform.rotation = storedRotation;
-            //image.enabled = true;
-            //isDisabled = false;
         }
     }
 }
