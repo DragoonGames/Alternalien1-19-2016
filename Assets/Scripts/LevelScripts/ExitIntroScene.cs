c86af4442928584a55f6f5afb6bda4015a9c2a87
Voltage2552 <passowt@gmatc.matc.edu>
2016-02-23 10:55:10 -0600

    Intro Sequence Fixed

--- Assets/Scripts/LevelScripts/ExitIntroScene.cs
+++ Assets/Scripts/LevelScripts/ExitIntroScene.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class ExitIntroScene : MonoBehaviour {
 
+	public Animator anim;
+	public float timer;
+	public float pauseTimer;
+
 	void Start(){
 		StartCoroutine (EndIntro());
+		//bkgEnd.GetComponent<SpriteRenderer>().enabled = false;
+		//anim = GetComponent<Animator> ();
 	}
 
 	IEnumerator EndIntro(){
 		print ("Intro Starts");
-		yield return new WaitForSeconds (3);
+		yield return new WaitForSeconds (timer);
+		anim.enabled = false;
+		yield return new WaitForSeconds (pauseTimer);
+		//bkgEnd.GetComponent<SpriteRenderer>().enabled = true;
+		//yield return new WaitForSeconds (3);
 		Application.LoadLevel(1);
 	}
 }
