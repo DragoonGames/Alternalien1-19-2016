069c209ef9881665f7e395803325c8b360154946
peck2703 <mrpeckjr@gmail.com>
2016-03-01 09:25:07 -0600

    Bug Fixes
    Cloud can kill his electrical field only now.
    
    Intro music fixed

--- Assets/Scripts/LevelScripts/ExitIntroScene.cs
+++ Assets/Scripts/LevelScripts/ExitIntroScene.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class ExitIntroScene : MonoBehaviour {
 
+    AudioSource myAudio;
+    public float delayAudio = 0;
+
 	void Start(){
 		StartCoroutine (EndIntro());
-	}
+        myAudio = GetComponent<AudioSource>();
+        myAudio.SetScheduledStartTime((double)delayAudio);
+        myAudio.Play();
 
-	IEnumerator EndIntro(){
+    }
+
+    IEnumerator EndIntro(){
 		print ("Intro Starts");
 		yield return new WaitForSeconds (3);
 		Application.LoadLevel(1);
